b3ab702fb2285ea3fb5e2f28899a76d2c7692ca5
Praise Ike <praiseike123@gmail.com>
2025-08-10 22:53:07 +0100

    update

--- app/Http/Controllers/Api/NotificationsController.php
+++ app/Http/Controllers/Api/NotificationsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Conversation;
 use Illuminate\Http\Request;
 
 class NotificationsController extends ApiController
@@ -10,7 +11,7 @@ class NotificationsController extends ApiController
     public function index(Request $request)
     {
         $user = auth()->user();
-        
+
         // Fetch notifications for the authenticated user
         $notifications = $user->notifications()->get([
             'id',
@@ -26,14 +27,17 @@ class NotificationsController extends ApiController
         return $this->respondWithSuccess('Notifications retrieved successfully', $notifications);
     }
 
-    public function getNotificationStats(){
+    public function getNotificationStats()
+    {
         $user = auth()->user();
 
         return $this->respondWithSuccess("Fetched notification stats", [
             'unread_count' => $user->notifications()->whereNull('read_at')->count(),
-            'unread_messages_count' => $user->conversations()->whereHas('messages', function ($query) {
-                $query->whereNull('read_at');
-            })->count(),
+            'unread_messages_count' => Conversation::where('user_id', $user->id)
+                ->orWhere('recipient_id', 'recipient_id')
+                ->whereHas('messages', function ($query) {
+                    $query->whereNull('read_at');
+                })->count(),
         ]);
     }
 }
